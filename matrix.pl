82c361c6ce9a1ce70b9aebd73f2f95147e0ba178
Alexandre Gautier <alexandre.gautier@epitech.eu>
2017-03-05 20:55:12 +0300

    Perl script to find best alignment of two sequences

---
+++ matrix.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+my $A = "-GAATTCAGTTA";
+my $B = "-GGATCGA";
+
+my $points = {
+	match 		=> 4,
+	mismatch	=> -3,
+	gap		=> -2,
+	gap_open	=> -2,
+	gap_extend	=> -1
+};
+
+my @matrix = ();
+
+sub init_matrix {
+	foreach my $i (0..length($A) - 1) {
+		@{ $matrix[$i] } = ();
+		foreach my $j (0..length($B) - 1) {
+			push @{ $matrix[$i] }, 0;
+		}
+	}
+
+	foreach my $i (0..length($A) - 1) {
+		$matrix[$i][0] = $i * $points->{gap};
+	}
+	foreach my $j (0..length($B) - 1) {
+		$matrix[0][$j] = $j * $points->{gap};
+	}
+}
+
+sub print_matrix {
+	print "   | ";
+	foreach my $j (0..length($B) - 1) {
+		my $cB = substr($B, $j, 1);
+		printf "% 3s ", $cB;
+	}
+	print "\n";
+
+	foreach my $i (0..length($A) - 1) {
+		my $cA = substr($A, $i, 1);
+		print "-" x (length($B) * 5 - 3), "\n";
+		print " $cA | ";
+
+		foreach my $j (0..length($B) - 1) {
+			my $n = $matrix[$i][$j];
+			printf "% 3d ", $n;
+		}
+		print "\n";
+	}
+}
+
+sub score_at {
+	my ($i, $j) = @_;
+
+	my $cA = substr($A, $i, 1);
+	my $cB = substr($B, $j, 1);
+	my $S = $points->{match};
+	$S = $points->{mismatch} if ($cA ne $cB);
+	return $S;
+}
+
+sub fill_matrix {
+	foreach my $i (1..length($A) - 1) {
+		foreach my $j (1..length($B) - 1) {
+			my $S = score_at($i, $j);
+
+			my $choice1 = $matrix[$i - 1][$j - 1] + $S;
+			my $choice2 = $matrix[$i - 1][$j] + $points->{gap};
+			my $choice3 = $matrix[$i][$j - 1] + $points->{gap};
+
+			my @choices = ($choice1, $choice2, $choice3);
+			my $max = (sort { $a <=> $b } @choices)[-1];
+			$matrix[$i][$j] = $max;
+		}
+	}
+}
+
+sub find_alignment {
+	my $alignmentA = "";
+	my $alignmentB = "";
+	my $totalScore = 0;
+	my $i = length($A) - 1;
+	my $j = length($B) - 1;
+	while ($i > 0 && $j > 0) {
+		my $score = $matrix[$i][$j];
+		my $scoreDiag = $matrix[$i - 1][$j - 1];
+		my $scoreUp = $matrix[$i][$j - 1];
+		my $scoreLeft = $matrix[$i - 1][$j];
+		my $cA = substr($A, $i, 1);
+		my $cB = substr($B, $j, 1);
+		my $S = score_at($i, $j);
+
+		if ($score == $scoreDiag + $S)
+		{
+			$alignmentA = $cA . $alignmentA;
+			$alignmentB = $cB . $alignmentB;
+			--$i;
+			--$j;
+		}
+		elsif ($score == $scoreLeft + $points->{gap})
+		{
+			$alignmentA = $cA . $alignmentA;
+			$alignmentB = "-" . $alignmentB;
+			--$i;
+		}
+		elsif ($score == $scoreUp + $points->{gap})
+		{
+			$alignmentA = "-" . $alignmentA;
+			$alignmentB = $cB . $alignmentB;
+			--$j;
+		}
+		$totalScore += $score;
+	}
+	return ($alignmentA, $alignmentB, $totalScore);
+}
+
+init_matrix();
+fill_matrix();
+
+print_matrix();
+print "\n";
+
+my ($alA, $alB, $total) = find_alignment();
+print "$alA\n$alB\n";
+print "Score: $total\n";
+
+# my $m = $matrix[-1][-1];
+# print "Max: $m\n";
+# my $i = length($A) - 1;
+# my $j = length($B) - 1;
+# my $m1 = $matrix[$i][$j];
+# print "Test[$i][$j] = $m1\n";
